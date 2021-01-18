9a589c2019fa421f05fa4054872344da2ddeadcd
nlee <nlee@Nick-2015-Air.local>
2021-01-17 22:42:22 -0700

    basic pokemon card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Pokemon } from "./types";
 import { useGetPokemon, useSortPokemon } from "./hooks";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import './App.css';
 
 function App() {
@@ -7,11 +8,33 @@ function App() {
     const sortedPokemon = useSortPokemon(pokemon, "name", true);
 
     return (
-        <ul>
-            {sortedPokemon.map((poke) => {
-                return <li>{poke.name}</li>
-            })}
-        </ul>
+        <div className="App">
+            <h1>Pokemon</h1>
+            <section>
+                {sortedPokemon.map((poke: Pokemon) => {
+                    return (
+                        <Card className="pokemon-card">
+                            <Card.Img variant="top" src={poke.img} />
+                            <Card.Body>
+                                <Card.Title>{poke.name}</Card.Title>
+                                <Card.Text>
+                                {poke.type.toString().replaceAll(",", ", ")}
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem>Cras justo odio</ListGroupItem>
+                                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                                <ListGroupItem>Vestibsectionum at eros</ListGroupItem>
+                            </ListGroup>
+                            <Card.Body>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    )
+                })}
+            </section>
+        </div>
     );
 }
 
